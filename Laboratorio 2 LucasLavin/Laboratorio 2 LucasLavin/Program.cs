cf367ed750df0ad2ee0c0b1dbdff65780c575e03
LucasLavin <ljlavin@miuandes.cl>
2019-08-16 15:07:49 +0200

    Primer Push

---
+++ Laboratorio 2 LucasLavin/Laboratorio 2 LucasLavin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio_2_LucasLavin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
